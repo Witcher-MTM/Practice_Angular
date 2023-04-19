2754079d53c80c2329c42fdefd08b3632c628fd9
Witcher-MTM <phantomit228@gmail.com>
2023-04-19 12:01:23 +0300

    Update zodiak.controller.js

--- server/Controller/zodiak.controller.js
+++ server/Controller/zodiak.controller.js
@@ -6,18 +6,18 @@ class ZodiakController {
         console.log("Month: " + month + "\nDay: " + day)
         const dateOfBirth = new Date(`${year}-${month}-${day}`);
         const zodiacs = [
-            { name: 'Овен', start: new Date(`2023-03-21`), end: new Date(`2023-04-19`) },
-            { name: 'Телец', start: new Date(`2023-04-20`), end: new Date(`2023-05-20`) },
-            { name: 'Близнецы', start: new Date(`2023-05-21`), end: new Date(`2023-06-21`) },
-            { name: 'Рак', start: new Date(`2023-06-22`), end: new Date(`2023-07-22`) },
-            { name: 'Лев', start: new Date(`2023-07-23`), end: new Date(`2023-08-22`) },
-            { name: 'Дева', start: new Date(`2023-08-23`), end: new Date(`2023-09-22`) },
-            { name: 'Весы', start: new Date(`2023-09-23`), end: new Date(`2023-10-23`) },
-            { name: 'Скорпион', start: new Date(`2023-10-24`), end: new Date(`2023-11-21`) },
-            { name: 'Стрелец', start: new Date(`2023-11-22`), end: new Date(`2023-12-21`) },
-            { name: 'Козерог', start: new Date(`2023-12-22`), end: new Date(`2024-01-19`) },
-            { name: 'Водолей', start: new Date(`2024-01-20`), end: new Date(`2024-02-18`) },
-            { name: 'Рыбы', start: new Date(`2024-02-19`), end: new Date(`2024-03-20`) },
+            { name: 'Овен', start: new Date(`${year}-03-21`), end: new Date(`${year}-04-19`) },
+            { name: 'Телец', start: new Date(`${year}-04-20`), end: new Date(`${year}-05-20`) },
+            { name: 'Близнецы', start: new Date(`${year}-05-21`), end: new Date(`${year}-06-21`) },
+            { name: 'Рак', start: new Date(`${year}-06-22`), end: new Date(`${year}-07-22`) },
+            { name: 'Лев', start: new Date(`${year}-07-23`), end: new Date(`${year}-08-22`) },
+            { name: 'Дева', start: new Date(`${year}-08-23`), end: new Date(`${year}-09-22`) },
+            { name: 'Весы', start: new Date(`${year}-09-23`), end: new Date(`${year}-10-23`) },
+            { name: 'Скорпион', start: new Date(`${year}-10-24`), end: new Date(`${year}-11-21`) },
+            { name: 'Стрелец', start: new Date(`${year}-11-22`), end: new Date(`${year}-12-21`) },
+            { name: 'Козерог', start: new Date(`${year}-12-22`), end: new Date(`${year}-01-19`) },
+            { name: 'Водолей', start: new Date(`${year}-01-20`), end: new Date(`${year}-02-18`) },
+            { name: 'Рыбы', start: new Date(`${year}-02-19`), end: new Date(`${year}-03-20`) },
         ];
         console.log(dateOfBirth);
         console.log(zodiacs);
